68bdf5721717573415a6c46c4a6de591ae1360e3
enzo69 <ethibert@edenschool.fr>
2020-06-29 17:08:42 +0200

    exam

--- src/componants/Appartement.js
+++ src/componants/Appartement.js
@@ -7,20 +7,19 @@ class Appart extends React.Component {
     constructor(props) {
         super(props);
     }
-    
-
     render() {
+        var price = this.props.price 
 
         return <div className="text-center col-md-4">
 
             <Card className="cardAppart">
-                <Card.Img className="col-md-12" variant="top" src={this.props.img} />
+                <img alt="imageAppartement" className="col-md-12 radiusImg" variant="top" src={this.props.img} />
                 <Card.Body className="col-md-12">
                     <Card.Title className="col-md-12 ralSemiBold adress">{this.props.address}</Card.Title>
                     <Card.Text className="col-md-12 playfairdisplay-r state">
                     {this.props.city} / {this.props.country}
                     </Card.Text>
-                    <Card.Text className="price helvetica" variant="primary">{'$'+this.props.price}</Card.Text>
+                    <Card.Text className="price helvetica" variant="primary">{'$'+price.splice(0,3,",")}</Card.Text>
                     <Card.Footer variant="primary helvetica">{this.props.sqfit+' Sq Ft'} • {this.props.bedroom+' Bedrooms'} • {this.props.bathroom+' bathroom'}</Card.Footer>
                 </Card.Body>
             </Card>
